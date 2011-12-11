cfff1f6636068291732414024411001e10331a74
TriVector.YX@gmail.com <TriVector.YX@gmail.com@07f3d810-1db1-2709-098d-da661ad60374>
2011-12-11 19:12:31 +0000

    commit after xcode upgrade

---
+++ ROS/ROS/de_yaoxinViewController.h
@@ -0,0 +1,13 @@
+//
+//  de_yaoxinViewController.h
+//  ROS
+//
+//  Created by 昕 姚 on 11.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface de_yaoxinViewController : UIViewController
+
+@end
---
+++ ROS/ROSTests/ROSTests.m
@@ -0,0 +1,32 @@
+//
+//  ROSTests.m
+//  ROSTests
+//
+//  Created by 昕 姚 on 11.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ROSTests.h"
+
+@implementation ROSTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ROSTests");
+}
+
+@end
